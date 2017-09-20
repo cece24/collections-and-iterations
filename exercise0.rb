6d3419f7f327adfb84aaec5c8c86bd786ab81787
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-20 18:24:26 -0400

    Exercise 0, add all arrays and hashes

---
+++ exercise0.rb
@@ -0,0 +1,31 @@
+# Arrays
+fav_colours = ["mint", "pink", "teal"]
+ages = [28, 32, 27]
+coin_flip_heads = ["heads", "heads", "tails", "heads", "tails"]
+fav_artists = ["Muse", "Ariana", "Calvin Harris"]
+fav_colours_symbols = [:mint, :pink, :teal]
+
+# Hashes
+word_definitions = {
+  "kitten" => "a young cat.",
+  "boop" => "poke a random person on the nose.",
+  "coffee" => "greatest beverage."
+}
+
+fav_movies = {
+  "The Lego Movie" => 2016,
+  "Jessica James" => 2017,
+  "Ratatouille" => 2007
+}
+
+city_populations = {
+  "Paris" => 2229621,
+  "Bangalore" => 8426000,
+  "Hong Kong" => 7347000
+}
+
+names = {
+  "Christine" => 30,
+  "Jeanine" => 28,
+  "Laura" => 27
+}
