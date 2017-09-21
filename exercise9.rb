c8228f1ffc8f3c7c1bc7581bc9e0724971da11dc
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-21 14:25:56 -0400

    Add 5% to cohort sizes, delete 2nd cohort and display

--- exercise9.rb
+++ exercise9.rb
@@ -15,3 +15,14 @@ display_cohorts(students)
 students[:cohort4] = 43
 
 puts students.keys
+
+# increase cohort sizes by 5%
+students.each do |name, size|
+  students[name] = size * 1.05
+end
+
+display_cohorts(students)
+
+# delete 2nd cohort and redisplay hash
+students.delete(:cohort2)
+display_cohorts(students)
