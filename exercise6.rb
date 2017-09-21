bb7aa0b5524d19d4e831bba6d791ff0574a2f40e
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-20 23:47:45 -0400

    Exercise 6, nested arrays and hashes

---
+++ exercise6.rb
@@ -0,0 +1,21 @@
+# part 1
+movies = {
+  1999 => ["The Matrix", "Star Wars: Episode 1", "The Mummy"],
+  2009 => ["Avatar", "Star Trek", "District 9"],
+  2019 => ["How to Train Your Dragon 3", "Toy Story 4", "Star Wars: Episode 9"]
+}
+
+# part 2
+phone_buttons = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  ["*", 0, "#"]
+]
+
+# part 3
+countries = [
+  { "name" => "France", "continent" => "Europe", "island" => "no"},
+  { "name" => "Singapore", "continent" => "Asia", "island" => "yes"},
+  { "name" => "Australia", "continent" => "Australia", "island" => "yes"}
+]
