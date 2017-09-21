470f8b5827c0bccf40f0cc436b507647fd02d8f6
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-21 13:57:14 -0400

    Add array of cohort names and sizes, define method to display list

---
+++ exercise9.rb
@@ -0,0 +1,13 @@
+students = {
+  :cohort1 => 34,
+  :cohort2 => 42,
+  :cohort3 => 22
+}
+
+def display_cohorts(list_of_cohorts)
+  list_of_cohorts.each do |name, size|
+    puts "#{name}: #{size} students"
+  end
+end
+
+display_cohorts(students)
