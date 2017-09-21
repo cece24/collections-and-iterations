fbb7349fb58766297d285e7d849c88b6fd883056
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-21 13:44:53 -0400

    Check if list contains bananas, display second list item

--- exercise8.rb
+++ exercise8.rb
@@ -11,3 +11,11 @@ grocery_list << "rice"
 display_list(grocery_list)
 
 puts "There are currently #{grocery_list.length} items on your list."
+
+if grocery_list.include?("bananas")
+  puts "You need to pick up bananas"
+else
+  puts "You don't need to pick up bananas today"
+end
+
+puts "Second item on list: #{grocery_list[1]}"
